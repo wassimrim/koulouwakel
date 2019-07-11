3da8eb034fd3d2cc1c0ecc0845f23841d45d7557
jourthoma <pronetoussama@hotmail.fr>
2019-07-11 12:08:36 +0100

    commentaire

--- src/main/java/com/projetb32/koulouwakel/entity/Family.java
+++ src/main/java/com/projetb32/koulouwakel/entity/Family.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
-
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/projetb32/koulouwakel/entity/Ingredient.java
+++ src/main/java/com/projetb32/koulouwakel/entity/Ingredient.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.io.Serializable;
 
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
