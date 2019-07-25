4d2eeac5fa7a1a4df5f2de605eb89f4ab819b57e
wassim rim <wassim_rim@hotmail.fr>
2019-07-25 23:49:41 +0100

    reglage de l'authorité de l'utilisateur au niveau de spring boot

--- src/main/java/com/projetb32/koulouwakel/security/WebSecurityConfig.java
+++ src/main/java/com/projetb32/koulouwakel/security/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                .antMatchers("/application/**").hasRole("ADMIN")
+                .antMatchers("/application/**").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
